9429d138d5548c37b2dbcf58e286306877a0827b
Gustavo Kasper <kaspergustavo27@hotmail.com>
2024-09-26 19:32:03 -0300

    estilizando o sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,22 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Title from '../../components/Title'
+
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/kaspergustavo.png" />
-    <Title fontSize={20}>Gustavo Kasper</Title>
+    <SidebarContainer>
+      <Avatar />
+      <Title fontSize={20}>Gustavo Kasper</Title>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        kaspergustavo
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Full Stack Developer
+      </Descricao>
+      <BotaoTema>Troca tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
